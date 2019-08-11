01e640f9c25e8339cfe9e4d67814973c87e2dcde
cgeorge42 <chadgeorge42@gmail.com>
2019-08-11 10:36:15 -0500

    update version

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter_web/material.dart';
 import 'package:tide_ui/graph_editor/icons/font_awesome_icons.dart';
 import 'graph_editor/graph_editor.dart';
 
-const String AppVersion = "0.02";
+const String AppVersion = "0.03";
 
 void main() => runApp(TheApp());
 
